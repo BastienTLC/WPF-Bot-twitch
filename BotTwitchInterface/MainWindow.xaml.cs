c1d431447963bc670ef9dc716b07a7f349fbaa3d
Goupil29Super <bastientalec@orange.fr>
2020-10-29 15:16:52 +0100

    Premiere version bot twitch avec interface wpf

---
+++ BotTwitchInterface/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Threading;
+using System.ComponentModel;
+
+namespace BotTwitchInterface
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string _botName = "bot_goupil";
+        private static string _broadcasterName = "gtp_goupil29";
+        private static string _twitchOAuth = "oauth:hykpdaxldn0rqyka5rohqwij6xpdzs";   //"oauth:xhjsna1pjxzm4p40w72hz1g5e5e7cg  hykpdaxldn0rqyka5rohqwij6xpdzs" // get chat bot's oauth from www.twitchapps.com/tmi/
+
+        string test="ou";
+        int i = 1;
+
+        IrcClient irc;
+        SerialPort sp;
+        BackgroundWorker bgworker = new BackgroundWorker();
+
+        public MainWindow()
+        {
+                InitializeComponent();
+            irc = new IrcClient("irc.twitch.tv", 6667,
+                        _botName, _twitchOAuth, _broadcasterName);
+
+            sp = new SerialPort();
+
+            PingSender ping = new PingSender(irc);
+            ping.Start();
+
+            Program _program = new Program(irc, sp, ref test);
+            AswerTchat.Text = "RIEN";
+
+            bgworker.DoWork += Bgworker_DoWork;
+            bgworker.ProgressChanged += Bgworker_ProgressChanged;
+            bgworker.WorkerReportsProgress = true;
+            bgworker.WorkerSupportsCancellation = true;
+        }
+
+        private void Bgworker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            char[] splitChars = { '#' };
+            string[] AnswerRep = test.Split(splitChars);
+            foreach(string x in AnswerRep)
+            {
+                AswerTchat.Text = x + "\n";
+                //AswerTchat.Text =  AswerTchat.Text + x + "\n";
+
+            }
+            cmp.Content = i;
+        }
+
+        private void Bgworker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+                while (true)
+                {
+                    bgworker.ReportProgress(i);
+                    Program _program = new Program(irc, sp, ref test);
+                    if (bgworker.CancellationPending)
+                    {
+                        break;
+                    }
+                    System.Threading.Thread.Sleep(100);
+                    i++;
+                 }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!bgworker.IsBusy)
+            {
+                bgworker.RunWorkerAsync();
+                Start.Content = "STOP";
+            }
+            else
+            {
+                bgworker.CancelAsync();
+                Start.Content = "START";
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //Message.Text = test;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BotTwitchInterface/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
+
+namespace BotTwitchInterface
+{
+   public class Program
+    {
+        private static string _broadcasterName = "gtp_goupil29";
+        private static string ColorMode = "off";
+        private static string ComValue = "COM13";
+        public string Couleur;
+        public string message;
+        public Program(IrcClient irc, SerialPort sp, ref string message2)
+        {
+
+            message = irc.ReadMessage();
+            //irc.SendPublicChatMessage(message);
+            if (message.Contains("PRIVMSG"))
+            {
+                message2 = message;
+            }
+            Console.WriteLine(message); // Print raw irc messages
+
+            if (message.Contains("PRIVMSG"))
+            {
+
+                int intIndexParseSign = message.IndexOf('!');
+                string userName = message.Substring(1, intIndexParseSign - 1);
+                intIndexParseSign = message.IndexOf(" :");
+                message = message.Substring(intIndexParseSign + 2);
+
+                if (userName.Equals(_broadcasterName) || (userName.Equals("wizebot")))
+                {
+
+                    if (message.Equals("!exitbot") || message.Contains("Le LIVE est OFFLINE"))
+                    {
+                        randomColor();
+                        irc.SendPublicChatMessage("Bye! Have a beautiful time!");
+                        RunExecutor("!off", userName);
+                        Environment.Exit(0);
+
+                    }
+                    else if (message.Contains("FOLLOW"))
+                    {
+                        randomColor();
+                        irc.SendPublicChatMessage("Bye! Have a beautiful time!");
+                        RunExecutor("!follow", userName);
+                    }
+                }
+
+
+                if (message.Equals("!info"))
+                {
+                    randomColor();
+                    irc.SendPublicChatMessage("/me imGlitch BOT QUI CHANGE LA COULEUR DES NEONS -> !infocommande imGlitch");
+                }
+
+                else if (message.Equals("!infocommande"))
+                {
+                    randomColor();
+                    irc.SendPublicChatMessage("/me PurpleStar !wave !cyclergb !multi !static !off !suprcar !flu !wip !fad !runing !meteo");
+                }
+
+                else if (message.Equals("!cyclergb") || (message.Equals("!off")) || (message.Equals("!wave") || (message.Equals("!multi"))))
+                {
+                    ColorMode = message;
+                    RunExecutor(ColorMode, userName);
+                }
+
+
+                else if (message.Contains("!static") || message.Contains("!suprcar") || message.Contains("!flu") || message.Contains("!wip") || message.Contains("!fad") || message.Contains("!runing") || message.Contains("!meteo"))
+                {
+
+                    try
+                    {
+                        ColorMode = message;
+                        string[] final = ColorMode.Split(' ');
+                        ColorMode = (final[0] + " " + final[1] + " " + final[2] + " " + final[3]);
+                        RunExecutor(ColorMode, userName);
+                    }
+                    catch
+                    {
+                        randomColor();
+                        ColorMode = message;
+                        irc.SendPublicChatMessage("/me " + message + " RRR GGG BBB ( MIN 0, MAX 255 ) ou message + !colorlist");
+
+                    }
+                }
+
+
+            }
+            //}
+
+            void RunExecutor(string _ColorMode, string sendComName)
+            {
+                try
+                {
+                    sp.PortName = ComValue;
+                    sp.BaudRate = 9600;
+                    sp.Open();
+
+                    if (sp.IsOpen)
+                    {
+                        sp.WriteLine(_ColorMode);
+                        while (sp.ReadLine() != _ColorMode)
+                        {
+                            sp.WriteLine(_ColorMode);
+                            Console.WriteLine(sp.ReadLine());
+                            Console.WriteLine(" ERROR");
+
+                        }
+                        Console.WriteLine(sp.ReadLine() + " SUCCES");
+                        irc.SendPublicChatMessage("/me " + sendComName + " Applique " + _ColorMode + " sur le neon");
+                        sp.Close();
+                    }
+
+                    else
+                    {
+                        sp.PortName = ComValue;
+                        sp.BaudRate = 9600;
+                        sp.Open();
+                    }
+
+
+                }
+                catch
+                {
+
+                    irc.SendPublicChatMessage("/me Une erreur est survenue. ");
+                }
+            }
+
+            void randomColor()
+            {
+
+                Random random = new Random();
+
+                string[] ColorArray = new string[] { "Blue", "BleuViolet", "CadetBlue", "Chocolate", "Coral", "DodgerBlue", "Firebrick", "GoldenRod", "Green", "HotPink" };
+                int numIndex = random.Next(ColorArray.Length);
+                irc.SendPublicChatMessage("/color " + ColorArray[numIndex]);
+
+
+            }
+
+        }
+    }
+}
